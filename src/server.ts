10c4b428764f6fcbd6e15de60f2b8fcb4821af35
gabrielsb18 <gbmodelator@gmail.com>
2024-08-31 22:04:09 -0300

    build: server app configured

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import RoutesMeasures from "./routes/routes.measures";
 
 const app = express();
-app.use(express.json());
 
-app.listen(3000, ()=> {
-    console.log("Servidor funcionando!")
-})
+app.use(express.json());
+app.use("/", RoutesMeasures);
+
+app.listen(process.env.PORT || 3001);
+
+
+export {app};
